18711489b86e622aa7c267fb4c58fb0b3aa4c7a9
eqsdxr <rxdsqe@gmail.com>
2025-02-15 18:05:48 +0500

    Create separate file for db logic

---
+++ app/db.py
@@ -0,0 +1,13 @@
+from typing import Generator
+
+from sqlmodel import Session, create_engine
+
+from app.config import main_config
+
+engine = create_engine(main_config.database_url, echo=True)
+
+def get_db_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+
