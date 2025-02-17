2137dee527c49173db3875d5b3b6bfa5862a6ad6
eqsdxr <rxdsqe@gmail.com>
2025-02-17 06:51:10 +0500

    Add utils for tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,16 +1,17 @@
 from collections.abc import Generator
 
 from pytest import fixture
+from sqlmodel import Session
 from fastapi.testclient import TestClient
-from sqlalchemy import Connection
 
+from app.db import engine
 from app.main import app
+from app.tests.utils import get_first_user_token_headers
 
 
-@fixture(scope="session")
-def db() -> Generator[Connection, None, None]:
-    with Connection(engine) as conn:  # TODO - Which engine should I put here?
-        # init_db(session)
+@fixture(scope="session", autouse=True)
+def db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
         yield session
 
 
@@ -18,3 +19,9 @@ def db() -> Generator[Connection, None, None]:
 def client() -> Generator[TestClient, None, None]:
     with TestClient(app) as c:
         yield c
+
+
+@fixture(scope="module")
+def superuser_token_headers(client: TestClient) -> dict[str, str]:
+    return get_first_user_token_headers(client)
+
---
+++ app/tests/utils.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+from fastapi.testclient import TestClient
+
+from app.config import main_config
+
+
+def random_lower_string() -> str:
+    return "".join(random.choices(string.ascii_lowercase, k=32))
+
+
+def random_email() -> str:
+    return f"{random_lower_string()}@{random_lower_string()}.com"
+
+
+def get_superuser_token_headers(client: TestClient) -> dict[str, str]:
+    login_data = {
+        "email": main_config.first_user_email,
+        "username": main_config.first_user_username,
+        "password": main_config.first_user_password,
+    }
+    r = client.post("api/login/access-token", data=login_data)
+    tokens = r.json()
+    a_token = tokens["access_token"]
+    headers = {"Authorization": f"Bearer {a_token}"}
+    return headers
