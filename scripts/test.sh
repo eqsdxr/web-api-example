f9d404e83627588730cd0f48135a8bf4b89cfe29
eqsdxr <rxdsqe@gmail.com>
2025-02-23 23:05:24 +0000

    Update test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+# Usage
+
+To run tests:
+- Create and activate virtual environment
+- Run scripts/test.sh
+```python
+bash scripts/test.sh
+```
+
+To build and run docker app in docker container:
+- Run scripts/build.sh
+```python
+bash scripts/build.sh
+```
+P.S. You may need to use `sudo bash scripts/build.sh`.
