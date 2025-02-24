fa10dcfa546d6d4e721cd7984a2638df9cedcc8d
eqsdxr <rxdsqe@gmail.com>
2025-02-24 22:52:39 +0500

    Add info and middleware into the application

--- app/config.py
+++ app/config.py
@@ -7,3 +7,28 @@ test_static_dir = Path().parent.parent / "app" / "tests" / "static"
 logger.add(
     stdout, colorize=True, format="{time} {level} {message}", level="INFO"
 )
+
+app_info = {
+    "title": "File metadata extractor API",
+    "description": "__No description.__",
+    "summary": "No summary.",
+    "version": "0.0.1",
+    "terms_of_service": "Nothing special.",
+    "contact": {"email": "default@example.com", "url": "broken-link"},
+    "license_info": {
+        "name": "Unlicense",
+        "identifier": "Unlicense",
+        "url": "https://unlicense.org/",
+    },
+}
+
+tags_metadata = [
+    {
+        "name": "metadata",
+        "description": "Metadata extracting operations.",
+        "external_docs": {
+            "description": "No description",
+            "url": "broken-link",
+        },
+    }
+]
--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import router
+from .config import app_info, tags_metadata
 
-app = FastAPI()
+app = FastAPI(**app_info, tags_metadata=tags_metadata, redoc_url=None)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
