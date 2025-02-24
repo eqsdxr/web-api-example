548705713c854aec7fea6499776c4d967a3dcb72
eqsdxr <rxdsqe@gmail.com>
2025-02-24 20:42:02 +0500

    Generalize metadata extracting, make it more safe

--- app/extractor.py
+++ app/extractor.py
@@ -1,14 +1,24 @@
 from typing import BinaryIO
 from PIL import Image
 from PIL.ExifTags import TAGS
+from fastapi import HTTPException, UploadFile
 
 
-def extract_image_metadata(img: BinaryIO) -> dict[str, str]:
+async def extract_image_metadata(img: BinaryIO) -> dict[str, str]:
     metadata = {}
-    image = Image.open(img)
-    exifdata = image.getexif()
-    for tagid in exifdata:
-        tagname = TAGS.get(tagid, tagid)
-        value = exifdata.get(tagid)
-        metadata[tagname] = value
+    try:
+        with Image.open(img) as image:
+            exifdata = image.getexif()
+            for tag_id in exifdata:
+                tag_name = TAGS.get(tag_id, tag_id)
+                metadata[tag_name] = exifdata.get(tag_id)
+    except Exception:
+        raise HTTPException(400, "Failed to process image")
     return metadata
+
+
+async def extract_metadata(file: UploadFile) -> dict[str, str]:
+    match file.content_type:
+        case "image/jpeg":
+            return await extract_image_metadata(file.file)
+    raise HTTPException(415, "Unsupported file type")
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, UploadFile
 
 from .models import MetadataResponse
-from .extractor import extract_image_metadata
+from .extractor import extract_metadata
 
 router = APIRouter()
 
 
 @router.post("/upload", response_model=MetadataResponse)
-async def upload_file(file: UploadFile):
+async def upload_file(file: UploadFile) -> MetadataResponse:
     metadata = {}
-    metadata["metadata"] = extract_image_metadata(file.file)
     metadata["filename"] = file.filename
     metadata["size"] = file.size
     metadata["content_type"] = file.content_type
+    metadata["metadata"] = await extract_metadata(file)
     metadata["hash"] = file.__hash__()
     return MetadataResponse(**metadata)
