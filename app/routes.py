e173fe6653cdc4b4671f56dd481577e05980fe6c
eqsdxr <rxdsqe@gmail.com>
2025-02-25 00:58:12 +0500

    Enhance upload endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from os import SEEK_END
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, Request, UploadFile, status
 
 from .models import MetadataResponse
 from .extractor import extract_metadata
@@ -11,8 +11,13 @@ router = APIRouter()
 
 
 @logger.catch  # Catch unexpected exceptions
-@router.post("/upload", response_model=MetadataResponse, tags=["metadata"])
-async def upload_file(file: UploadFile) -> MetadataResponse:
+@router.post(
+    "/upload",
+    response_model=MetadataResponse,
+    status_code=status.HTTP_200_OK,
+    tags=["metadata"],
+)
+async def upload_file(file: UploadFile, request: Request) -> MetadataResponse:
     file_size = file.file.seek(0, SEEK_END)
     await file.seek(0)  # Reset pointer position
     metadata = {
@@ -22,4 +27,9 @@ async def upload_file(file: UploadFile) -> MetadataResponse:
         "metadata": await extract_metadata(file),
         "hash": await calculate_sha256(file),
     }
+    if not request.client:
+        host = "Unknown"
+    else:
+        host = request.client.host
+    logger.info(f"{host}; {file_size}; {file.filename}; {file.content_type}")
     return MetadataResponse(**metadata)
