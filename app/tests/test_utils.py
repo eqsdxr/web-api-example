0726e6dd41eb07436a6a07630155e75b5c42bff7
eqsdxr <rxdsqe@gmail.com>
2025-02-25 10:30:32 +0500

    Enhance tests

--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -7,11 +7,11 @@ from ..config import get_settings
 
 @pytest.mark.anyio
 async def test_calculate_sha256():
+    expected_img_hash = (
+        "24b3a9e4b92d9f253b9510ed42e4d33a5ac39cdf3e96b53e8e3c4e7146ea5491"
+    )
     with open(get_settings().test_static_dir / "test_img1.jpg", "rb") as f:
         file = UploadFile(f)
-        img_hash = await calculate_sha256(file)
-    assert isinstance(img_hash, str)
-    assert (
-        img_hash
-        == "24b3a9e4b92d9f253b9510ed42e4d33a5ac39cdf3e96b53e8e3c4e7146ea5491"
-    )
+        actual_img_hash = await calculate_sha256(file)
+    assert isinstance(actual_img_hash, str)
+    assert actual_img_hash == expected_img_hash
