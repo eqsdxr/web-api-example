a56b49fcfb3b51007a5e6ba81698ad0a58440764
eqsdxr <rxdsqe@gmail.com>
2025-03-09 12:14:27 +0500

    feat: add current user reading endpoint

--- app/routes/v1/user.py
+++ app/routes/v1/user.py
@@ -1,3 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request, status
 
-user_router = APIRouter(prefix="/user")
+from app.config import limiter, logger
+from app.deps import CurrentUser, get_current_user
+from app.models import UserResponse
+
+user_router = APIRouter(prefix="/users", tags=["user"])
+
+
+@logger.catch  # Catch unexpected exceptions
+@user_router.get(
+    "/me",
+    response_model=UserResponse,
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(get_current_user)],
+)
+@limiter.limit("5/minute")
+async def get_user_me(
+    current_user: CurrentUser,
+    request: Request,
+) -> UserResponse:
+    _ = request
+    return UserResponse(**current_user.model_dump())
