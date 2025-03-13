5ec4359f8c4ce46463fbbd84805b64be5f7cd7b7
eqsdxr <rxdsqe@gmail.com>
2025-03-13 17:58:37 +0500

    chore: replace '_' with '-' in endpoint path for consistency

--- backend/app/routes/v1/private.py
+++ backend/app/routes/v1/private.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
-
-from app.models import UserCreate
 from app.crud import create_user
 from app.deps import SessionDep
+from app.models import UserCreate
+from fastapi import APIRouter
 
 private_router = APIRouter(prefix="/private", tags=["private"])
 
-@private_router.post("/create_user")
+
+@private_router.post("/create-user")
 async def create_user_private(session: SessionDep, user: UserCreate):
     created_user = create_user(session, user)
     return created_user
