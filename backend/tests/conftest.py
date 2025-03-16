c73fc45b714ca5fd4fcc146fc7a6e86a87e221da
eqsdxr <rxdsqe@gmail.com>
2025-03-16 11:34:58 +0500

    refactor(test): simplify conftest.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,7 +7,6 @@ from sqlmodel import Session
 from app.db import (
     engine,
 )
-from app.deps import get_db
 from app.main import app as app
 from tests.utils import get_superuser_token_headers, init_db
 
@@ -18,20 +17,12 @@ def client() -> Generator[TestClient, None, None]:
         yield c
 
 
-def get_test_db() -> Generator[Session, None, None]:
+@fixture(scope="session", autouse=True)
+def db_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         init_db(session)
         yield session
         session.commit()
-
-
-@fixture(scope="function")
-def db_session() -> Generator[Session, None, None]:
-    yield from get_test_db()
-
-
-# Override with one which creates tables and a superuser
-app.dependency_overrides[get_db] = get_test_db
 
 
 @fixture(scope="function")
